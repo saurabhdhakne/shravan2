4245a4f903deb68b065803a7e8ea1a856102a787
saurabhdhakne <sbdhake309@gmail.com>
2021-10-13 10:41:02 +0530

    new carousel addd

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,3 +5,18 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  $(function () { 
+    var header = $(".classinitial"); 
+    $(window).scroll(function () { 
+        var scroll = $(window).scrollTop(); 
+
+        if (scroll >= 100) { 
+            header.removeClass('classinitial') 
+                    .addClass("classfinal"); 
+        } else { 
+            header.removeClass("classfinal") 
+                    .addClass('classinitial'); 
+        } 
+    }); 
+}); 
